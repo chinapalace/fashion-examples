fbc41f26928cfc77f76860127b6572b2701d3794
Justin McLaren <justinmclaren@Justins-MacBook-Pro.local>
2018-03-13 15:10:10 -0700

    deploy

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    name: `Justin McLaren`,
+    title: `Fashion Design Work`,
+    date: `March 13, 2018`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-smartypants`]
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `slides`,
+        path: `${__dirname}/src/pages/`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-postcss-sass`,
+      options: {
+        postCssPlugins: [],
+        precision: 8
+      }
+    }
+  ]
+};
